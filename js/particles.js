1756d50a02adef5b251d65dacaa998a3ea416a48
AyoBakre <ayobakre@gmail.com>
2020-12-08 11:00:37 +0100

    design template update

--- js/particles.js
+++ js/particles.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 40,
         "density": {
           "enable": true,
           "value_area": 800
