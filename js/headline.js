f92fd5578f38ea7832d74fcba01bc901cf163ad7
AyoBakre <ayobakre@gmail.com>
2021-01-31 17:18:08 +0100

    portfolio design

---
+++ js/headline.js
@@ -0,0 +1,25 @@
+/* Typewriter effect on the heading description */
+
+
+"use strict";
+var i = 0;
+var txt = "I'm a Software Developer_";
+var speed = 100;
+
+function typeWriter() {
+  	if (i < txt.length) {
+    	document.getElementById("type").innerHTML += txt.charAt(i);
+    	i++;
+    	setTimeout(typeWriter, speed);
+  	}
+}
+
+document.body.onload = function() {
+ typeWriter()
+}
+
+
+$(".menu, nav ul li a").on("click", function(e) {
+  e.preventDefault();
+  $("nav ul").toggleClass("active");
+});
